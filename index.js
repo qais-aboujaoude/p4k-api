82eb4897741107989b5dd7b1948d613276ce912a
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-18 20:59:39 +0200

    Feature: Return JS Object with all needed details
    TODO:
    1- Decouple the code into seperate files
    2- Clean up the code and try to reduce promises
    3- Link it up with an express API

--- index.js
+++ index.js
@@ -18,17 +18,41 @@ const getAlbumReview = (artist, album) => {
       .catch(err => console.log(err))
   })
 }
+const pitchfork = () => {
+  return new Promise((resolve, reject) => {
+    getAlbumReview('radiohead', 'a moon shaped pool')
+      .then(url => {
+        axios.get(`${baseURL}${url[0]}`)
+        .then(r => {
+          let $ = cheerio.load(r.data)
+          return resolve({
+            score: $('span.score').text(),
+            reviewURL: `${baseURL}${url[0]}`,
+            reviewTitle: $('.review-detail__abstract').text(),
+            reviewBody: $('.review-detail__text').text()
+          })
+        })
+      })
+  })
+}
 
-getAlbumReview('radiohead', 'a moon shaped pool').then(r => console.log(r))
+pitchfork().then(r => console.log(r.reviewBody))
 
-// axios.get(`${baseURL}/search/?query=${artist}+${album}`)
-//   .then(r => {
-//     let links = []
-//     let $ = cheerio.load(r.data)
-//     $('.results-fragment').find('.review__link').each((i, el) => {
-//       let link = $(el).attr('href')
-//       links.push(link)
-//     }) 
-//     console.log(links)
-//   })
-//   .catch(err => console.log(err))
+/*
+const pitchfork = () => {
+  return new Promise((resolve, reject) => {
+    getAlbumReview('radiohead', 'a moon shaped pool')
+      .then(r => {
+        let x = return axios.get(baseURL + r)
+        console.log(x)
+        axios.get(baseURL + r)
+          .then(r => {
+            let $ = cheerio.load(r.data)
+            return resolve({
+              score: $('span.score').text(),
+            })
+          })
+          .catch(err => console.log(err))
+      })
+  })
+} */
