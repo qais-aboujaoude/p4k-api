2d0de7f6a8b7c5485622a583ce07f36e561621d4
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-18 19:08:45 +0200

    Refactor: Turn href scraping into a function

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const axios   = require('axios'),
       cheerio = require('cheerio')
       baseURL = 'http://pitchfork.com'
 let selec = 'ul.results-fragment'
-let artist = 'migos'
-let album  = 'culture'
 
-axios.get(`${baseURL}/search/?query=${artist}+${album}`)
-  .then(r => {
-    let links = []
-    let $ = cheerio.load(r.data)
-    $('.results-fragment').find('.review__link').each((i, el) => {
-      let link = $(el).attr('href')
-      links.push(link)
-    })
-    console.log(links)
+const getAlbumReview = (artist, album) => {
+  return new Promise((resolve, reject) => {
+    return axios.get(`${baseURL}/search/?query=${artist}+${album}`)
+      .then(r => {
+        let links = []
+        let $ = cheerio.load(r.data)
+        $('.results-fragment').find('.review__link').each((i, el) => {
+          let link = $(el).attr('href')
+          links.push(link)
+        }) 
+        return resolve(links)
+      })
+      .catch(err => console.log(err))
   })
-  .catch(err => console.log(err))
+}
+
+getAlbumReview('radiohead', 'a moon shaped pool').then(r => console.log(r))
+
+// axios.get(`${baseURL}/search/?query=${artist}+${album}`)
+//   .then(r => {
+//     let links = []
+//     let $ = cheerio.load(r.data)
+//     $('.results-fragment').find('.review__link').each((i, el) => {
+//       let link = $(el).attr('href')
+//       links.push(link)
+//     }) 
+//     console.log(links)
+//   })
+//   .catch(err => console.log(err))
